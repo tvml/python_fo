acf00d9e753d398bb34fe8cd32c434749cd65422
pogo-opossum <g.gambosi@gmail.com>
2020-11-29 11:07:12 +0100

    ll1 parser

---
+++ parse/ll/ll1_parser.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Implementation of parsing through LL(1) parser."""
+
+import base.base as base
+import parse.base.parser_exceptions as pre
+
+import grammar.cf.cf_grammar as cfg
+import tools.tools as tools
+
+
+class LL1_parser(base.Base):
+    """A LL(1) parser."""
+
+    def __init__(self, grammar):
+        """Initialize."""
+        self.parse_table = self.derive_parse_table(grammar)
+        if self.parse_table is None:
+            raise Exception('Grammar is not LL(1)')
+
+
+    def parse(self, list_of_tokens):
+        """Parse a sentence (string)"""
+        # TODO
+
+    def derive_parse_table(self, grammar):
+        """Derive a LL(1) parse table."""
+        # TODO
+        return parse_table
+
+    def __str__(self):
+        """Return a string representation of the parser."""
+        s = ''
+        # TODO
+        return s
+
+    def __repr__(self):
+        """Associate a description of the object to its identifier."""
+        # TODO
+        return None
